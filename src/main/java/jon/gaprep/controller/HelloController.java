21b1289baa65acfafe786a4f845c33af32bf9cc9
joneikholmkea <jone@kea.dk>
2022-12-15 13:21:55 +0100

    test3

--- src/main/java/jon/gaprep/controller/HelloController.java
+++ src/main/java/jon/gaprep/controller/HelloController.java
@@ -3,14 +3,9 @@ package jon.gaprep.controller;
 import jon.gaprep.model.Greeting;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.sql.SQLException;
 import java.util.Date;
-
 
 @RestController
 public class HelloController {
@@ -20,7 +15,7 @@ public class HelloController {
         System.out.println("greeting controller Java message - december 2022");
         Greeting g = new Greeting("hi from server at Azure", new Date());
         return new ResponseEntity<>(g, HttpStatus.OK);
-        // Jackson (from Spring Boot Web) is used to convert object to JSON
+        // Jackson (from Spring Boot Web) is used to convert object to JSON ...
     }
 
 
