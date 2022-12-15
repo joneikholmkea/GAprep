5f5e438cf73cc969e43b584421bd4c21f34ef9d6
joneikholmkea <jone@kea.dk>
2022-12-15 13:25:52 +0100

    test3

--- src/main/java/jon/gaprep/controller/HelloController.java
+++ src/main/java/jon/gaprep/controller/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 
     @GetMapping("/")
     public ResponseEntity<Greeting> getHello(){
-        System.out.println("greeting controller Java message - december 2022");
+        System.out.println("greeting controller Java message - december 15 2022");
         Greeting g = new Greeting("hi from server at Azure", new Date());
         return new ResponseEntity<>(g, HttpStatus.OK);
         // Jackson (from Spring Boot Web) is used to convert object to JSON ...
